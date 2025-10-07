5faf5a7df122f6ec128bffb245181cbea9cb93a1
mahmoud karimi <m.karimi@Ahuan.tehran>
2025-10-07 13:14:01 +0330

    change route

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../views/Home.vue';
-import Albums from '../views/albums.vue';
+import Albums from '../views/Albums.vue';
 const routes = [
   { path: '/', 
     name:'Home',
